fbcf90e5b2d11d19997282be78cbe1d45be1faa5
flaviare1s <flaviarster@gmail.com>
2026-01-29 07:31:10 -0300

    Ajusta grid

--- src/components/ProductsList/styles.ts
+++ src/components/ProductsList/styles.ts
@@ -1,19 +1,19 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
-  display: flex;
-  flex-wrap: wrap;
-  align-items: stretch;
-  justify-content: flex-start;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 320px);
+  justify-content: center;
+  justify-items: start;
   gap: 2rem;
 
   max-width: 1240px;
+  width: 100%;
   height: 100%;
   margin: 4rem auto;
   padding: 0 2rem;
 
   & > * {
-    flex: 0 320px;
-
+    width: 100%;
   }
-`
+`;
